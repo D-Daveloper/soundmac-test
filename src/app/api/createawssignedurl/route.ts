626bc9774404ac65c314597fee9294ece8170fbe
D-Daveloper <davidmuoegbunam@gmail.com>
2026-01-29 18:13:29 +0100

    reduced the expires time for the signed url

--- src/app/api/createawssignedurl/route.ts
+++ src/app/api/createawssignedurl/route.ts
@@ -96,7 +96,7 @@ export async function POST(req: Request) {
     });
   
     const uploadUrl = await getSignedUrl(s3, command, {
-      expiresIn: 180,
+      expiresIn: 60,
     });
     const query = {
       user: user._id,
